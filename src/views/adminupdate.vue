0583c244670058264ad196ebdfb9da0b743c3451
BOOK-SAMA <120651112+BOOK-SAMA@users.noreply.github.com>
2024-02-11 20:34:46 +0700

    add dat and status

--- src/views/adminupdate.vue
+++ src/views/adminupdate.vue
@@ -35,7 +35,11 @@
                   <h6 class="mb-0">ชื่อภาษาไทย</h6>
                 </div>
                 <div class="col-md-9 pe-5">
-                  <input v-model="THAIname" class="form-control form-control-lg" placeholder="" />
+                  <input
+                    v-model="THAIname"
+                    class="form-control form-control-lg"
+                    placeholder=""
+                  />
                 </div>
               </div>
               <hr class="mx-n3" />
@@ -45,7 +49,11 @@
                   <h6 class="mb-0">ชื่อภาษาอังกฤษ</h6>
                 </div>
                 <div class="col-md-9 pe-5">
-                  <input v-model="ENGname" class="form-control form-control-lg" placeholder="" />
+                  <input
+                    v-model="ENGname"
+                    class="form-control form-control-lg"
+                    placeholder=""
+                  />
                 </div>
               </div>
               <hr class="mx-n3" />
@@ -55,7 +63,12 @@
                   <h6 class="mb-0">ชื่อเล่น</h6>
                 </div>
                 <div class="col-md-9 pe-5">
-                  <textarea v-model="nickname" class="form-control" rows="3" placeholder=""></textarea>
+                  <textarea
+                    v-model="nickname"
+                    class="form-control"
+                    rows="3"
+                    placeholder=""
+                  ></textarea>
                 </div>
               </div>
 
@@ -63,25 +76,64 @@
 
               <div class="row align-items-center py-3">
                 <div class="col-md-3 ps-5">
-                  <h6 class="mb-0">วันเดือนปีเกิด</h6>
+                  <h5 class="mb-0">วันเดือนปีเกิด</h5>
                 </div>
-                <div class="col-md-9 pe-5">
-                  
-                  
+                <div class="col-md-3 pe-2">
+                  <h6 class="mb-0">วัน</h6>
+                  <select v-model="day" class="form-control">
+                    <option v-for="d in days" :key="d">{{ d }}</option>
+                  </select>
                 </div>
-                
+                <div class="col-md-3 pe-2">
+                  <h6 class="mb-0">เดือน</h6>
+                  <select v-model="month" class="form-control">
+                    <option v-for="m in months" :key="m">{{ m }}</option>
+                  </select>
+                </div>
+                <div class="col-md-3 pe-5">
+                  <h6 class="mb-0">ปีที่เกิด</h6>
+                  <select v-model="year" class="form-control">
+                    <option v-for="y in years" :key="y">{{ y }}</option>
+                  </select>
+                </div>
+
                 <!-- ช่องวันเกิด ใช้ date แทน @-->
                 <hr class="mx-n3" />
-
-
-
-
-                <hr class="mx-n3" />
-                <div class="px-5 py-4">
-                  <button type="submit" class="btn btn-primary btn-lg" @click="updateSubmit(this.$route.params.id)">
-                    Submit
-                  </button>
+              </div>
+              <hr class="mx-n3" />
+              <div class="row align-items-center py-3">
+                <div class="col-md-3 ps-5">
+                  <h6 class="mb-0">สถานะของผู้สมัคร</h6>
                 </div>
+                <div class="col-md-9 pe-5">
+                  <select
+                    v-model="status"
+                    class="custom-select my-1 mr-sm-2"
+                    id="inlineFormCustomSelectPref"
+                  >
+                    <option selected disabled>
+                      เคยเป็นนักศึกษา/เป็นผู้สำเร็จการศึกษา/เป็นนักศึกษา...
+                    </option>
+                    <option value="เคยเป็นนักศีกษา">เคยเป็นนักศีกษา</option>
+                    <option value="เป็นผู้สำเร็จการศึกษา">
+                      เป็นผู้สำเร็จการศึกษา
+                    </option>
+                    <option value="เป็นนักศึกษาปัจจุบัน">
+                      เป็นนักศึกษาปัจจุบัน
+                    </option>
+                  </select>
+                </div>
+              </div>
+
+              <hr class="mx-n3" />
+              <div class="px-5 py-4">
+                <button
+                  type="submit"
+                  class="btn btn-primary btn-lg"
+                  @click="updateSubmit(this.$route.params.id)"
+                >
+                  Submit
+                </button>
               </div>
             </div>
           </div>
@@ -102,14 +154,15 @@ export default {
 
   data() {
     return {
-      formattedDate: "",
+      dateText: "",
       Username: "",
       Password: "",
+
       THAIname: "",
       ENGname: "",
       Oldname: "",
       nickname: "",
-      dateofbirth: ref(new Date()),
+
       status: "",
       academicstatus: "",
       academicnumber: "",
@@ -130,11 +183,16 @@ export default {
       role: "",
       paystatus: "",
       submitt: "",
+      days: Array.from({ length: 31 }, (_, i) => i + 1),
+      months: Array.from({ length: 12 }, (_, i) => i + 1),
+      years: Array.from(
+        { length: 100 },
+        (_, i) => new Date().getFullYear() - i
+      ),
     };
   },
   computed: {
     // ใช้ computed property เพื่อ return ค่าที่ได้จาก formatdate ที่มีการรับ dateofbirth เป็นพารามิเตอร์
-   
   },
   mounted() {
     console.log(this.$route.params.id);
@@ -158,7 +216,9 @@ export default {
           this.ENGname = res.data.thing.Engname;
           this.Oldname = res.data.thing.Oldname;
           this.nickname = res.data.thing.Nickname;
-          this.formattedDate = res.data.thing.Dateofbirth;
+          const dateString = res.data.thing.Dateofbirth;
+          [this.day, this.month, this.year] = dateString.split("/");
+          this.status = res.data.thing.Status;
 
           // this.ENGname = res.data.thing.ENGname;
           // this.nickname = res.data.thing.Nickname;
@@ -222,7 +282,6 @@ export default {
         })
         .catch();
     },
-    
   },
 };
 </script>
