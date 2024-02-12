1a42ef3f54022a85a85ef167dbd0ec8046133adb
BOOK-SAMA <120651112+BOOK-SAMA@users.noreply.github.com>
2024-02-12 11:48:23 +0700

    not yet add image

--- src/views/adminupdate.vue
+++ src/views/adminupdate.vue
@@ -35,7 +35,11 @@
                   <h6 class="mb-0">ชื่อภาษาไทย</h6>
                 </div>
                 <div class="col-md-9 pe-5">
-                  <input v-model="THAIname" class="form-control form-control-lg" placeholder="" />
+                  <input
+                    v-model="THAIname"
+                    class="form-control form-control-lg"
+                    placeholder=""
+                  />
                 </div>
               </div>
               <hr class="mx-n3" />
@@ -45,7 +49,11 @@
                   <h6 class="mb-0">ชื่อภาษาอังกฤษ</h6>
                 </div>
                 <div class="col-md-9 pe-5">
-                  <input v-model="ENGname" class="form-control form-control-lg" placeholder="" />
+                  <input
+                    v-model="ENGname"
+                    class="form-control form-control-lg"
+                    placeholder=""
+                  />
                 </div>
               </div>
               <hr class="mx-n3" />
@@ -55,7 +63,12 @@
                   <h6 class="mb-0">ชื่อเล่น</h6>
                 </div>
                 <div class="col-md-9 pe-5">
-                  <textarea v-model="nickname" class="form-control" rows="3" placeholder=""></textarea>
+                  <textarea
+                    v-model="nickname"
+                    class="form-control"
+                    rows="3"
+                    placeholder=""
+                  ></textarea>
                 </div>
               </div>
 
@@ -93,7 +106,11 @@
                   <h6 class="mb-0">สถานะของผู้สมัคร</h6>
                 </div>
                 <div class="col-md-9 pe-5">
-                  <select v-model="status" class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                  <select
+                    v-model="status"
+                    class="custom-select my-1 mr-sm-2"
+                    id="inlineFormCustomSelectPref"
+                  >
                     <option selected disabled>
                       เคยเป็นนักศึกษา/เป็นผู้สำเร็จการศึกษา/เป็นนักศึกษา...
                     </option>
@@ -114,33 +131,85 @@
                   <h6 class="mb-0">ระดับปริญญาตรี สาขาวิชาเอก</h6>
                 </div>
                 <div class="col-md-9 pe-5">
-                  <textarea v-model="academicstatus" class="form-control" rows="1"></textarea>
+                  <textarea
+                    v-model="academicstatus"
+                    class="form-control"
+                    rows="1"
+                  ></textarea>
                 </div>
               </div>
 
               <hr class="mx-n3" />
-              <img v-if="profileimage" :src="profileimage" alt="Preview" class="rounded-circle p-1" width="110" />
-              <!-- Display a default image if previewFile is not available -->
-              <p v-else>
-                <img src="http://www.scsualumni.net/images/logo/resize-1482551623803.png" alt="Admin"
-                  class="rounded-circle p-1" width="110" />
-              </p>
 
+              <div class="row align-items-center py-3">
+                <div class="col-md-3 ps-5">
+                  <h6 class="mb-0">รุปถ่ายของผู้ใช้</h6>
+                </div>
+                <div class="col-md-9 pe-5">
+                  <h6 class="mb-0">รูปที่แสดงอยู่นี้เป็นไฟล์รูปใน database </h6>
+                  หากต้องการเปลี่ยนแปลงสามารถ upload ไฟล์ได้ด้านล่าง
+                  <img
+                    v-if="profileimage"
+                    :src="profileimage"
+                    alt="Preview"
+                    class="rounded-circle p-1"
+                    width="200"
+                  />
+                  <!-- Display a default image if previewFile is not available -->
+                  <p v-else>
+                    <img
+                      src="http://www.scsualumni.net/images/logo/resize-1482551623803.png"
+                      alt="Admin"
+                      class="rounded-circle p-1"
+                      width="200"
+                    />
+                  </p>
+                  <input
+                    ref="fileInput"
+                    class="form-control form-control-lg"
+                    id="formFileLg"
+                    type="file"
+                    @change="handleprofile"
+                  />
+                  <div class="small text-muted mt-2">
+                    (นามสกุลไฟล์ .jpg ไม่เกิน 10 MB)
+                  </div>
+                </div>
+              </div>
               <hr class="mx-n3" />
 
-              <img v-if="payimage" :src="payimage" alt="Preview" class="rounded-circle p-1" width="110" />
-              <!-- Display a default image if previewFile is not available -->
-              <p v-else>
-                <img src="http://www.scsualumni.net/images/logo/resize-1482551623803.png" alt="Admin"
-                  class="rounded-circle p-1" width="110" />
-              </p>
-
-
+              <div class="row align-items-center py-3">
+                <div class="col-md-3 ps-5">
+                  <h6 class="mb-0">ใบเสร็จชำระเงินสมาชิกลำดับต่างๆ</h6>
+                </div>
+                <div class="col-md-9 pe-5">
+                  <img
+                    v-if="payimage"
+                    :src="payimage"
+                    alt="Preview"
+                    class=""
+                    width="400"
+                  />
+                  <!-- Display a default image if previewFile is not available -->
+                  <p v-else>
+                    <img
+                      src="http://www.scsualumni.net/images/logo/resize-1482551623803.png"
+                      alt="Admin"
+                      class="rounded-circle p-1"
+                      width="400"
+                    />
+                  </p>
+                </div>
+              </div>
 
               <hr class="mx-n3" />
 
               <div class="px-5 py-4">
-                <button type="submit" class="btn btn-primary btn-lg" @click="updateSubmit(this.$route.params.id)">
+                <button
+                  type="submit"
+                  class="btn btn-primary btn-lg"
+                  @click="updateSubmit(this.$route.params.id)"
+                >
                   Submit
                 </button>
               </div>
@@ -179,14 +248,8 @@ export default {
       masterdegreenumber: "",
       doctordegree: "",
       doctordegreenumber: "",
-      address: "",
-      phonenumber: "",
-      idline: "",
-      email: "",
-      job: "",
-      jobposition: "",
-      levelmember: "",
-      levelmemberthing: "",
+
+     
       statusinfo: "",
       pnstatus: "",
       role: "",
@@ -198,10 +261,10 @@ export default {
         { length: 100 },
         (_, i) => new Date().getFullYear() - i
       ),
+
+      file:null,
       profileimage: null,
       payimage: null,
-
-
     };
   },
   computed: {
@@ -210,8 +273,8 @@ export default {
   async mounted() {
     console.log(this.$route.params.id);
     await this.getuserdata(this.$route.params.id);
-    await this.downloadImageAndDisplay(localStorage.getItem("uuidprofile"))
-    await this.downloadpayImageAndDisplay(localStorage.getItem("uuidpayimage"))
+    await this.downloadImageAndDisplay(localStorage.getItem("uuidprofile"));
+    await this.downloadpayImageAndDisplay(localStorage.getItem("uuidpayimage"));
   },
   methods: {
     async getuserdata(id) {
@@ -241,21 +304,8 @@ export default {
           this.masterdegreenumber = res.data.thing.Masterdegreenumber;
           this.doctordegree = res.data.thing.Doctoraldegree;
           this.doctordegreenumber = res.data.thing.Doctoraldegreenumber;
-          localStorage.setItem("uuidprofile", res.data.thing.Image)
-          localStorage.setItem("uuidpayimage", res.data.thing.Payimage)
-        })
-        .catch();
-    },
-    updateSubmit(id) {
-      axios
-        .post(`${import.meta.env.VITE_API}update/${id}`, {
-          username: this.Username,
-          password: this.Password,
-
-          thainame: this.THAIname,
-        })
-        .then((res) => {
-          console.log(res);
+          localStorage.setItem("uuidprofile", res.data.thing.Image);
+          localStorage.setItem("uuidpayimage", res.data.thing.Payimage);
         })
         .catch();
     },
@@ -269,20 +319,27 @@ export default {
               Authorization: "Bearer " + localStorage.getItem("tokenstring"),
               "Content-Type": "application/json",
             },
-            responseType: 'arraybuffer', // Set the responseType to 'arraybuffer' to handle binary data
+            responseType: "arraybuffer", // Set the responseType to 'arraybuffer' to handle binary data
           }
         );
 
         // Convert the binary data to a data URL
-        const imageSrc = `data:${response.headers['content-type']};base64,${btoa(new Uint8Array(response.data).reduce((data, byte) => data + String.fromCharCode(byte), ''))}`;
+        const imageSrc = `data:${
+          response.headers["content-type"]
+        };base64,${btoa(
+          new Uint8Array(response.data).reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            ""
+          )
+        )}`;
 
         // Display the image using the data URL
-        const imgElement = document.getElementById('your-image-id'); // Replace 'your-image-id' with the actual ID of your image element
+        const imgElement = document.getElementById("your-image-id"); // Replace 'your-image-id' with the actual ID of your image element
         if (imgElement) {
           imgElement.src = imageSrc;
         }
         this.profileimage = imageSrc;
-        console.log('Image downloaded and displayed.');
+        console.log("Image downloaded and displayed.");
       } catch (error) {
         console.error("Error downloading image:", error);
         localStorage.removeItem("userid");
@@ -295,26 +352,33 @@ export default {
       try {
         // Fetch the image content from the server
         const response = await axios.get(
-          `${import.meta.env.VITE_API2}admin/preview/${uuid}`,
+          `${import.meta.env.VITE_API2}admin/previewpayimage/${uuid}`,
           {
             headers: {
               Authorization: "Bearer " + localStorage.getItem("tokenstring"),
               "Content-Type": "application/json",
             },
-            responseType: 'arraybuffer', // Set the responseType to 'arraybuffer' to handle binary data
+            responseType: "arraybuffer", // Set the responseType to 'arraybuffer' to handle binary data
           }
         );
 
         // Convert the binary data to a data URL
-        const imageSrc = `data:${response.headers['content-type']};base64,${btoa(new Uint8Array(response.data).reduce((data, byte) => data + String.fromCharCode(byte), ''))}`;
+        const imageSrc = `data:${
+          response.headers["content-type"]
+        };base64,${btoa(
+          new Uint8Array(response.data).reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            ""
+          )
+        )}`;
 
         // Display the image using the data URL
-        const imgElement = document.getElementById('your-image-id'); // Replace 'your-image-id' with the actual ID of your image element
+        const imgElement = document.getElementById("your-image-id"); // Replace 'your-image-id' with the actual ID of your image element
         if (imgElement) {
           imgElement.src = imageSrc;
         }
         this.payimage = imageSrc;
-        console.log('Image downloaded and displayed.');
+        console.log("Image downloaded and displayed.");
       } catch (error) {
         console.error("Error downloading image:", error);
         localStorage.removeItem("userid");
@@ -322,7 +386,58 @@ export default {
         localStorage.removeItem("uuid");
         router.push({ path: "/login" });
       }
-    }
+    },
+    updateSubmit(id) {
+      const URL = `${import.meta.env.VITE_API2}register2`;
+      let data = new FormData();
+
+      data.append("username", this.username);
+      data.append("password", this.password);
+      data.append("confirmpassword", this.confirmpassword);
+
+      data.append("thainame", this.thainame);
+      data.append("engname", this.engname);
+      data.append("oldname", this.oldname);
+      data.append("nickname", this.nickname);
+      data.append("dateofbirth", this.date);
+
+      data.append("status", this.status);
+      data.append("academicstatus", this.academicstatus);
+      data.append("academicnumber", this.academicnumber);
+      data.append("masterdegree", this.masterdegree);
+      data.append("masterdegreenumber", this.masterdegreenumber);
+      data.append("doctordegree", this.doctordegree);
+      data.append("doctordegreenumber", this.doctordegreenumber);
+
+      data.append("address", this.address);
+      data.append("phonenumber", this.phonenumber);
+      data.append("phonemail", this.phonemail);
+      data.append("idline", this.Idline);
+      data.append("email", this.email);
+      data.append("job", this.job);
+      data.append("jobposition", this.jobposition);
+      data.append("jobaddress", this.jobadress);
+      data.append("levelmember", this.levelmember);
+      data.append("levelmemberthing", this.levelmemberthing);
+      data.append("statusinfo", this.statusinfo);
+      data.append("file", this.file);
+      data.append("payimage", this.payimage);
+      data.append("PNstatus", this.pnstatus);
+      let config = {
+        header: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+
+      axios.post(URL, data, config).then((response) => {
+        // console.log("this is res => ", this.date);
+        console.log("this is res => ", response);
+        router.push({ path: "/" });
+      });
+    },
+    handleprofile(event) {
+      this.file = event.target.files[0];
+    },
   },
 };
 </script>
