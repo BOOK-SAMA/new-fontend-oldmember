5059d2cbdecea9bba7ef37aaee8f92d7244d4be4
BOOK-SAMA <120651112+BOOK-SAMA@users.noreply.github.com>
2024-02-27 20:23:13 +0700

    add some package

--- src/components/cart.vue
+++ src/components/cart.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h2>Shopping Cart (from cart.vue)</h2>
     <ul>
       <section class="h-100 h-custom" style="background-color: #eee;">
         <div class="container py-5 h-100">
@@ -11,15 +10,13 @@
 
                   <div class="row">
 
-                    <div class="col-lg-7">
-                      <h5 class="mb-3"><a href="#!" class="text-body"><i
-                            class="fas fa-long-arrow-alt-left me-2"></i>Continue shopping</a></h5>
-                      <hr>
+                    <div class="col">
+
 
                       <div class="d-flex justify-content-between align-items-center mb-4">
                         <div>
                           <p class="mb-1">Shopping cart</p>
-                          <p class="mb-0">You have 4 items in your cart</p>
+                          <p class="mb-0">You have {{ cartLength }} items in your cart</p>
                         </div>
                         <div>
                           <p class="mb-0"><span class="text-muted">Sort by:</span> <a href="#!" class="text-body">price
@@ -27,24 +24,23 @@
                         </div>
                       </div>
                       <li v-for="item in cart" :key="item.id">
-                        <!-- {{ item.name }} - ${{ item.price }} -->
                         <div class="card mb-3 mb-lg-0">
                           <div class="card-body">
                             <div class="d-flex justify-content-between">
                               <div class="d-flex flex-row align-items-center">
                                 <div>
-                                  <img
-                                  :src="item.image"
-                                    class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
+                                  <img :src="item.image" class="img-fluid rounded-3" alt="Shopping item"
+                                    style="width: 65px;">
                                 </div>
                                 <div class="ms-3">
                                   <h5> {{ item.name }}</h5>
-                                  <p class="small mb-0">text</p>
                                 </div>
                               </div>
                               <div class="d-flex flex-row align-items-center">
                                 <div style="width: 50px;">
-                                  <h5 class="fw-normal mb-0">1</h5>
+                                  <h5 class="fw-normal mb-0">{{ this.thing }}</h5>
+                                  <button @click="increment"> + </button>
+                                  <button @click="decrement"> - </button>
                                 </div>
                                 <div style="width: 80px;">
                                   <h5 class="mb-0">{{ item.price }} บาท</h5>
@@ -56,66 +52,19 @@
                         </div>
                       </li>
                     </div>
-                    <div class="col-lg-5">
-
-                      <div class="card bg-primary text-white rounded-3">
-                        <div class="card-body">
-                          <div class="d-flex justify-content-between align-items-center mb-4">
-                            <h5 class="mb-0">Card details</h5>
-                            <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
-                              class="img-fluid rounded-3" style="width: 45px;" alt="Avatar">
-                          </div>
-
-                          <p class="small mb-2">Card type</p>
-                          <a href="#!" type="submit" class="text-white"><i
-                              class="fab fa-cc-mastercard fa-2x me-2"></i></a>
-                          <a href="#!" type="submit" class="text-white"><i class="fab fa-cc-visa fa-2x me-2"></i></a>
-                          <a href="#!" type="submit" class="text-white"><i class="fab fa-cc-amex fa-2x me-2"></i></a>
-                          <a href="#!" type="submit" class="text-white"><i class="fab fa-cc-paypal fa-2x"></i></a>
-
-                          <form class="mt-4">
-                            <div class="form-outline form-white mb-4">
-                              <input type="text" id="typeName" class="form-control form-control-lg" siez="17"
-                                placeholder="Cardholder's Name" />
-                              <label class="form-label" for="typeName">Cardholder's Name</label>
-                            </div>
-
-                            <div class="form-outline form-white mb-4">
-                              <input type="text" id="typeText" class="form-control form-control-lg" siez="17"
-                                placeholder="1234 5678 9012 3457" minlength="19" maxlength="19" />
-                              <label class="form-label" for="typeText">Card Number</label>
-                            </div>
+                    <div class="card bg-primary text-white rounded-3">
+                      <div class="card-body">
 
 
-
-                          </form>
-
-                          <hr class="my-4">
-
+                        <button type="button" class="btn btn-info btn-block btn-lg">
                           <div class="d-flex justify-content-between">
-                            <p class="mb-2">Subtotal</p>
-
+                            <div>ยืนยันการสั่งซื้อ </div>
                           </div>
+                        </button>
 
-                          <div class="d-flex justify-content-between">
-                            <p class="mb-2">Shipping</p>
-                          </div>
-
-                          <div class="d-flex justify-content-between mb-4">
-                            <p class="mb-2">Total(Incl. taxes)</p>
-                          </div>
-
-                          <button type="button" class="btn btn-info btn-block btn-lg">
-                            <div class="d-flex justify-content-between">
-                              <span>price</span>
-                              <span>Checkout <i class="fas fa-long-arrow-alt-right ms-2"></i></span>
-                            </div>
-                          </button>
-
-                        </div>
                       </div>
-
                     </div>
+
 
                   </div>
 
@@ -134,7 +83,21 @@
 export default {
   props: {
     cart: Array,
+    thing: 1 ,
   },
-};
+  computed: {
+    cartLength() {
+      return this.cart.length;
+    }
+  },
+  increment() {
+    this.$emit('update:item', { ...this.item, quantity: this.item.quantity + 1 });
+  },
+  decrement() {
+    if (this.item.quilete > 0) {
+      this.$emit('update:item', { ...this.item, quantity: this.item.quantity - 1 });
+    }
+  }
+}
 </script>
   
