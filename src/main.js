d1727c07d71dc23645556038cdc8c91971dae71c
BOOK-SAMA <120651112+BOOK-SAMA@users.noreply.github.com>
2024-03-03 22:10:36 +0700

    maybe it work

--- src/main.js
+++ src/main.js
@@ -8,11 +8,23 @@ import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 import axios from "axios"
 import { createPinia } from "pinia"
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 const pinia = createPinia()
 const app = createApp(App)
+
+
 app.component('VueDatePicker', VueDatePicker);
 app.config.globalProperties.$axios = axios;
 app.use(pinia)
 app.use(router)
+app.use(vuetify)
 app.mount('#app')
