e37f728708dbd99ceae8d43454c946d42da784b4
BOOK-SAMA <120651112+BOOK-SAMA@users.noreply.github.com>
2024-04-06 20:08:38 +0700

    ทำการเปลี่ยนหน้า notfound

--- src/views/register.vue
+++ src/views/register.vue
@@ -528,7 +528,8 @@ export default {
         // console.log("this is res => ", this.date);
         this.responseStatus = response.status
         console.log("this is res => ", response);
-        window.location.href = "https://mytestsilpakorn.azurewebsites.net/";
+        // window.location.href = "https://mytestsilpakorn.azurewebsites.net/";
+        
       }).catch((error) => {
         console.log("this is error => ", error);
         router.push({ path: "/:notfound" });
