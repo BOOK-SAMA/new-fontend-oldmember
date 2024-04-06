f8658556f41b6a7653a533eebdab83973697e03e
BOOK-SAMA <120651112+BOOK-SAMA@users.noreply.github.com>
2024-04-06 20:18:17 +0700

    แก้ปุ่ม logout จาก loginadmin ไป loginpage

--- src/product/producttool.vue
+++ src/product/producttool.vue
@@ -91,7 +91,7 @@ export default {
       localStorage.removeItem("userid");
       localStorage.removeItem("tokenstring");
       localStorage.removeItem("uuid");
-      router.push({ path: "/loginadmin" });
+      router.push({ path: "/login" });
     },
     dodelete(id) {
       axios
