7c0d97d1901f98187881b7c50ec6fa14c0aaa458
e <sakulma_s@silpakorn.edu>
2024-04-07 21:46:51 +0700

    vpsconfig to deploy

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-
+  server: {
+    port: 5173, // กำหนดพอร์ตของเซิร์ฟเวอร์เป็น 5173
+    host: '45.136.255.192', // กำหนด IP address หรือโฮสต์ที่เซิร์ฟเวอร์จะใช้ เช่น 'localhost' หรือ '0.0.0.0'
+    // เปิดเบราว์เซอร์โดยอัตโนมัติเมื่อเริ่มเซิร์ฟเวอร์
+  }
   
 
 
