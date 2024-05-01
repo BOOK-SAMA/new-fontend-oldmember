cf9867bb27c632202f997f25ca444d80f126891d
sakulma <sakulma_s@silpakorn.edu>
2024-05-01 14:16:23 +0700

    [edit] - แก้ไข ui หน้า ข้อมูลสมาชิกสมาคมศิษย์เก่า มหาลัยศิลปากร

--- src/views/admintoo.vue
+++ src/views/admintoo.vue
@@ -76,7 +76,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">แก้ไขรหัสผ่าน</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close" >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
@@ -91,7 +91,7 @@
           <p v-else>Passwords do not match!</p>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-primary" :disabled="!passwordsMatch">บันทึกรหัสผ่าน</button>
+          <button type="button" class="btn btn-primary text-white" :disabled="!passwordsMatch">บันทึกรหัสผ่าน</button>
           <button type="button" class="btn btn-secondary" data-dismiss="modal">
             ปิด
           </button>
