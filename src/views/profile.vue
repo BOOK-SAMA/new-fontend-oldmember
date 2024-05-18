379472827d40c1e68edb87d17a574ca08777e546
BOOK-SAMA <120651112+BOOK-SAMA@ss17.ss18@gmail.com>
2024-05-19 00:29:04 +0700

    ปัญหาเกี่ยวกับ

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -1,85 +1,5 @@
 <template>
-	<!-- <div class="contrainer" style="padding: 1.5cm;">
-		<div class="row ">
-			<div class="row">
-				<nav class="navbar navbar-expand navbar-dark fixed-top" style=" background-color: #F9CC02;">
-					<ul class="navbar-nav mr-auto p-2">
-						<li class="nav-item active mr-4">
-							<a class="btn bg-dark  text-dark" data-toggle="collapse" href="#collapseExample"
-								role="button">
-								<span class="navbar-toggler-icon"></span
-							</a>
-						</li>
-						<li class="nav-item active">
-							<a class="nav-link text-dark" href="https://mytestsilpakorn.azurewebsites.net/">หน้าแรก</a>
-						</li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle text-dark" href="#" data-toggle="dropdown"
-								aria-haspopup="true">
-								เกี่ยวกับสมาคม
-							</a>
-							<div class="dropdown-menu " style=" background-color: #F9CC02;">
-								<a class="dropdown-item text-dark" href="#">สาส์นจากนายกสมาคมฯ</a>
-								<a class="dropdown-item text-dark" href="#">ข้อมูลทั่วไป</a>
-								<a class="dropdown-item text-dark" href="#">ตราสัญลักษณ์สมาคม</a>
-								<a class="dropdown-item text-dark" href="#">การสมัครสมาชิก</a>
-								<a class="dropdown-item text-dark" href="#">การสมัครสมาชิก</a>
-								<a class="dropdown-item text-dark" href="#">คณะกรรมการสมาคมฯ</a>
-								<a class="dropdown-item text-dark" href="#">สมาชิกสมาคมฯ</a>
-							</div>
-						</li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle text-dark" href="#" data-toggle="dropdown"
-								aria-haspopup="true">
-								ข้อมูลศิษย์เก่า
-							</a>
-							<div class="dropdown-menu " style=" background-color: #F9CC02;">
-								<a class="dropdown-item text-dark" href="#">ทำเนียบศิษย์เก่า</a>
-								<a class="dropdown-item text-dark" href="#">บัณฑิตคณะวิทยาศาสตร์ มศก.</a>
-								<a class="dropdown-item text-dark" href="#">ศิษย์เก่าดีเด่น</a>
-								<a class="dropdown-item text-dark" href="#">เกี่ยวกับอัลบั้ม</a>
-								<a class="dropdown-item text-dark" href="#">เกี่ยวกับรายชื่อตัวแทนรุ่น</a>
-							</div>
-						</li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle text-dark" href="#" data-toggle="dropdown"
-								aria-haspopup="true">
-								กิจกรรม
-							</a>
-							<div class="dropdown-menu " style=" background-color: #F9CC02;">
-								<a class="dropdown-item text-dark" href="#">กิจกรรม 2566</a>
-								<a class="dropdown-item text-dark" href="#">กิจกรรม 2561</a>
-								<a class="dropdown-item text-dark" href="#">กิจกรรม ปี 2558 - 2560</a>
-							</div>
-						</li>
-						<li class="nav-item active">
-							<a class="nav-link text-dark" href="#">สินค้าที่ระลึก</a>
-						</li>
-						<li class="nav-item active">
-							<a class="nav-link text-dark" href="#">ผู้สนับสนุนเวบไซต์สมาคมฯ</a>
-						</li>
-						<li class="nav-item active">
-							<a class="nav-link text-dark" href="#">เว็บบอร์ด</a>
-						</li>
-						<li class="nav-item active">
-							<a class="nav-link text-dark" href="#">ข่าวสารทั่วไป</a>
-						</li>
-						<li class="nav-item active">
-							<a class="nav-link text-dark" href="#">แจ้งชำระเงิน</a>
-						</li>
-						<li class="nav-item active">
-							<a class="nav-link text-dark" href="#">ติดต่อเรา</a>
-						</li>
-					</ul>
-					<div class="form-inline my-2 my-lg-0">
-						<button class="btn my-2 my-sm-0" type="submit" @click="handlelogout()">
-							ออกจากระบบ
-						</button>
-					</div>
-				</nav>
-			</div>
-		</div>
-	</div> -->
+
 
 	<nav class="sidebar">
 		<a href="#" class="logo">SUSAA</a>
@@ -275,6 +195,7 @@
 <style>
 @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css");
 
+
 * {
 	margin: 0;
 	padding: 0;
@@ -305,7 +226,6 @@
 	width: 100%;
 	margin-top: 40px;
 	overflow-y: scroll;
-
 }
 
 .menu-item {
@@ -324,7 +244,6 @@
 .item a,
 .submenu-item {
 	padding: 16px;
-	/* background-color: red; */
 	display: inline-block;
 	width: 100%;
 	border-radius: 12px
@@ -426,16 +345,26 @@
 	left: 0;
 	width: 100%;
 }
+
 .profile-card {
-  width: 400px; /* ตั้งค่าความกว้าง */
-  height: 400px; /* ตั้งค่าความยาวให้เท่ากับความกว้าง */
-  background-color: #e7f2fe; /* สีพื้นหลัง */
-  border-radius: 10px; /* ขอบมน */
-  display: flex; /* ทำให้สามารถใช้ justify-content และ align-items ได้ */
-  justify-content: center; /* จัดกึ่งกลางในแนวนอน */
-  align-items: center; /* จัดกึ่งกลางในแนวตั้ง */
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* เงา */
-  margin: 50px; /* ระยะห่างจากองค์ประกอบอื่น */
+	width: 400px;
+	/* ตั้งค่าความกว้าง */
+	height: 400px;
+	/* ตั้งค่าความยาวให้เท่ากับความกว้าง */
+	background-color: #e7f2fe;
+	/* สีพื้นหลัง */
+	border-radius: 10px;
+	/* ขอบมน */
+	display: flex;
+	/* ทำให้สามารถใช้ justify-content และ align-items ได้ */
+	justify-content: center;
+	/* จัดกึ่งกลางในแนวนอน */
+	align-items: center;
+	/* จัดกึ่งกลางในแนวตั้ง */
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	/* เงา */
+	margin: 50px;
+	/* ระยะห่างจากองค์ประกอบอื่น */
 }
 </style>
 
@@ -468,7 +397,7 @@ export default {
 					menu.classList.remove("submenu-active");
 				})
 			})
-			sideBarClose.addEventListener("click", () => sideBar.classList.toggle("close"))
+			sideBarClose.addEventListener("click", () =>  sideBar.classList.toggle("close"))
 
 			console.log(menuItems, subMenuTitles);
 		});
