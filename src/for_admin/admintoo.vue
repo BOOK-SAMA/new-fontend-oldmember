30a2af5a43f64972e92ca2913b38ea66f11f0194
BOOK-SAMA <120651112+BOOK-SAMA@ss17.ss18@gmail.com>
2024-06-20 20:29:31 +0700

    modified:   src/for_admin/admintoo.vue

--- src/for_admin/admintoo.vue
+++ src/for_admin/admintoo.vue
@@ -9,23 +9,23 @@
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="https://mytestsilpakorn.azurewebsites.net/">หน้าแรก <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="https://mytestsilpakorn.azurewebsites.net/">หน้าแรก <span
+              class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/admincreate">สร้างผู้ใช้</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link"
-            href="https://mytestsilpakorn.azurewebsites.net/wp-admin/">แก้ไขหน้าเว็บ</a>
+          <a class="nav-link" href="https://mytestsilpakorn.azurewebsites.net/wp-admin/">แก้ไขหน้าเว็บ</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" :href=" `/seeallproduct/${this.$route.params.id}`">จัดการสินค้า</a>
+          <a class="nav-link" :href="`/seeallproduct/${this.$route.params.id}`">จัดการสินค้า</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" :href=" `/seeallorder`">ดูรายการสั่งซื้อต่างๆ</a>
+          <a class="nav-link" :href="`/seeallorder`">ดูรายการสั่งซื้อต่างๆ</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" :href=" `/seeallfrom`">ดูรายการแจ้งชำระเงินต่างๆ</a>
+          <a class="nav-link" :href="`/seeallfrom`">ดูรายการแจ้งชำระเงินต่างๆ</a>
         </li>
       </ul>
       <form class="form-inline my-2 my-lg-0">
@@ -36,78 +36,48 @@
     </div>
   </nav>
 
-<div class="container table-responsive py-5"> 
-<table class="table table-bordered table-hover">
-  <thead class="thead-dark">
-      <tr>
-        <th scope="col ">#</th>
-        <th scope="col">Username</th>
-        <th scope="col">Thainame</th>
-        <th scope="col">ENGname</th>
-        <th scope="col">PhoneNumber</th>
-        <th scope="col">Role</th>
-        <th scope="col">Paystatus</th>
-        <th scope="col">Accressstatus</th>
-    <th scope="col">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(users, index) in users" :key="index">
-        <td v-text="users.ID"></td>
-        <td v-text="users.Username"></td>
-        <td v-text="users.Thainame"></td>
-        <td v-text="users.Engname"></td>
-        <td v-text="users.Phonenumber"></td>
-        <td v-text="users.Role"></td>
-        <td v-text="users.Paystatus"></td>
-        <td v-text="users.Accessstatus"></td>
-        <td class="p-1">
-          <router-link :to="{ path: '/update/' + users.ID + '/edit' }"
-            class="btn btn-success btn-sm">แก้ไข</router-link>
-          <div class="b-example-divider p-1"></div>
+  <div class="container table-responsive py-5">
+    <table class="table table-bordered table-hover">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col ">#</th>
+          <th scope="col">Username</th>
+          <th scope="col">Thainame</th>
+          <th scope="col">ENGname</th>
+          <th scope="col">PhoneNumber</th>
+          <th scope="col">Role</th>
+          <th scope="col">Paystatus</th>
+          <th scope="col">Accressstatus</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(users, index) in users" :key="index">
+          <td v-text="users.ID"></td>
+          <td v-text="users.Username"></td>
+          <td v-text="users.Thainame"></td>
+          <td v-text="users.Engname"></td>
+          <td v-text="users.Phonenumber"></td>
+          <td v-text="users.Role"></td>
+          <td v-text="users.Paystatus"></td>
+          <td v-text="users.Accessstatus"></td>
+          <td class="p-1">
+            <router-link :to="{ path: '/update/' + users.ID + '/edit' }"
+              class="btn btn-success btn-sm">แก้ไข</router-link>
+            <div class="b-example-divider p-1"></div>
 
-          <router-link :to="{ path: '/admintoo/' + this.$route.params.id }" class="btn btn-success btn-sm"
-            @click="dodelete(users.ID)">ลบ
-          </router-link>
-          <div class="b-example-divider p-1"></div>
+            <router-link :to="{ path: '/admintoo/' + this.$route.params.id }" class="btn btn-success btn-sm"
+              @click="dodelete(users.ID)">ลบ
+            </router-link>
+            <div class="b-example-divider p-1"></div>
 
-          <a class="btn btn-success btn-sm" :href="state ? '#' : `/orderhistory/${users.ID}`">ดูประวัติการสั่งซื้อ</a>
+            <a class="btn btn-success btn-sm" :href="state ? '#' : `/orderhistory/${users.ID}`">ดูประวัติการสั่งซื้อ</a>
 
-        </td>
-      </tr>
-    </tbody>
-  </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-  <!-- Modal -->
-  <!-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">แก้ไขรหัสผ่าน</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close" >
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <label for="newpassword" class="mr-2">New Password:</label>
-          <input type="password" v-model="newpassword" @input="comparePasswords">
-          <hr class="mx-n3" />
-          <label for="comfirepassword" class="mr-2">Confirm Password:</label>
-          <input type="password" v-model="comfirepassword" @input="comparePasswords">
-          <hr class="mx-n3" />
-          <p v-if="passwordsMatch">Passwords match!</p>
-          <p v-else>Passwords do not match!</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-primary text-white" :disabled="!passwordsMatch">บันทึกรหัสผ่าน</button>
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">
-            ปิด
-          </button>
-        </div>
-      </div>
-    </div>
-  </div> -->
 </template>
 
 
@@ -151,8 +121,8 @@ export default {
         .then((res) => {
           if (res.status == 200) {
             console.log(res);
-            alert("ทำการลบ ผู้ใช้งาน เรียบร้อยแล้ว")
-            router.push({ path: "/admintoo/" + responseData.userid });
+            alert("ทำการลบ ผู้ใช้งาน เรียบร้อยแล้ว");
+            window.location.reload(); 
           }
         })
         .catch();
@@ -224,18 +194,3 @@ export default {
 
 
 
-<!-- <div class="">
-    <tr v-for="(users, index) in users" :key="index">
-      <td v-text="users.ID"></td>
-      <td v-text="users.Username"></td>
-      <td v-text="users.Thainame"></td>
-      <td v-text="users.ENGname"></td>
-      <td v-text="users.PhoneNumber"></td>
-      <td v-text="users.Role"></td>
-      <td v-text="users.Paystatus"></td>
-      <td>
-        <button class="btn btn-success btn-sm">แก้ไข</button>
-        <button class="btn btn-success btn-sm">ลบ</button>
-      </td>
-    </tr>
-  </div> -->
