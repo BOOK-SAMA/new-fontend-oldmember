c8fb170b7fa6e2444526f6014e911a8d2ce818f4
BOOK-SAMA <120651112+BOOK-SAMA@ss17.ss18@gmail.com>
2024-06-23 18:57:10 +0700

    modified:   src/for_users/userorder-detail.vue

--- src/for_users/userorder-detail.vue
+++ src/for_users/userorder-detail.vue
@@ -22,7 +22,7 @@
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
-                <button class="btn btn-secondary my-2 my-sm-0" type="submit" @click="handlelogout()">
+                <button class="btn  my-2 my-sm-0" type="submit" @click="handlelogout()">
                     ออกจากระบบ
                 </button>
             </form>
