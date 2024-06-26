3becb7d8da0a3587b0b4430c1e9ada936dfd9a32
sakulma <sakulma_s@silpakorn.edu>
2024-06-26 15:40:18 +0700

    modified:   src/for_admin/updateorder.vue

--- src/for_admin/updateorder.vue
+++ src/for_admin/updateorder.vue
@@ -90,7 +90,6 @@
                     <div class="myaccounthalfcolumn">
                         <div class="container">
                             <p align="right"><strong>ราคาทั้งหมด</strong></p><br>
-                           
                             <p align="right">ราคาค่าส่ง : 100 บาท</p>
                             <p align="right">ราคารวม : <span>{{ parseInt(orders.totalCartPrice) }}</span></p>
                         </div>
