404f1e2f87cb4718f6b1c03d15b011fe6be7895d
sakulma <sakulma_s@silpakorn.edu>
2024-06-26 15:41:07 +0700

    modified:   src/for_admin/updateorder.vue

--- src/for_admin/updateorder.vue
+++ src/for_admin/updateorder.vue
@@ -29,7 +29,7 @@
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
-                <button class="btn btn-secondary my-2 my-sm-0" type="submit" @click="handlelogout()">
+                <button class="btn  my-2 my-sm-0" type="submit" @click="handlelogout()">
                     ออกจากระบบ
                 </button>
             </form>
@@ -176,7 +176,7 @@ export default {
 			localStorage.removeItem("userid");
 			localStorage.removeItem("tokenstring");
 			localStorage.removeItem("uuid");
-			 window.location.href = "https://graduation-silpakorn.azurewebsites.net/";
+			window.location.href = "https://graduation-silpakorn.azurewebsites.net/";
 		},
         async Getorder(uniqueorder) {
             try {
