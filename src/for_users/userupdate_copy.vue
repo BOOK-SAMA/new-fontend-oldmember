c1873448c19cc08c88757e3c62960f7553ecc78d
BOOK-SAMA <120651112+BOOK-SAMA@ss17.ss18@gmail.com>
2024-06-27 19:35:57 +0700

    changes 	modified:   src/for_users/userupdate_copy.vue

--- src/for_users/userupdate_copy.vue
+++ src/for_users/userupdate_copy.vue
@@ -696,7 +696,7 @@ export default {
         data.append("masterdegree", this.state.masterdegree);
         data.append("masterdegreenumber", this.state.masterdegreenumber);
         data.append("doctordegree", this.state.doctordegree);
-        data.append("Doctoraldegreenumber", this.state.doctordegreenumber);
+        data.append("doctoraldegreenumber", this.state.doctordegreenumber);
         data.append("phonemail", this.state.Phonemail);
         data.append("idline", this.state.Idline);
         data.append("job", this.state.job);
